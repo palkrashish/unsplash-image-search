48d27e9c34b5020aecc2164927c0a0fe2abda36c
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-06-23 13:07:20 +0530

    feat:- creating form and button and filter based on input

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
-import './App.css'
-const App = ()=>{
-  return(
-    <div>
-    <h1> Unsplash Image Search </h1>
+import { Form } from "react-bootstrap";
+import { useRef } from "react";
+import "./App.css";
+const App = () => {
+  const searchInput = useRef(null);
+  const handleSearch = (event) => {
+    event.preventDefault();
+    console.log(searchInput.current.value);
+  };
+  const handleSelection = (selection) => {
+    searchInput.current.value = selection;
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title">Image Search</h1>
+      <div className="search-section">
+        <Form onSubmit={handleSearch}>
+          <Form.Control
+            type="search"
+            placeholder="Type something to search..."
+            className="search-input"
+            ref={searchInput}
+          />
+        </Form>
+      </div>
+      <div className="filters">
+        <div onClick={() => handleSelection("nature")}>Nature</div>
+        <div onClick={() => handleSelection("birds")}>Birds</div>
+        <div onClick={() => handleSelection("cats")}>Cats</div>
+        <div onClick={() => handleSelection("shoes")}>Shoes</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
